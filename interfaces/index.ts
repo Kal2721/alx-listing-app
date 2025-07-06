d2a31885bf42f2b25e19a1e4194a7cfe16545f78
kalkidan <kalkidanteshale6@gmail.com>
2025-07-06 14:06:10 +0300

    Props to customize buttons and property listings

---
+++ interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface CardProps = {
+  image: string[];
+  title: string;
+  location: string;
+  price: string;
+  navigation: string;
+  rating: string;
+  icon0: string;
+  icon1: string;
+  icon2: string;
+  icon3: string;
+  number0: string;
+  number1: string;
+  number2: string;
+  number3: string;
+};
+
+export interface buttonProps= {
+	children: React.ReactNode;
+};
+
